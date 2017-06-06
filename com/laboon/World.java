e986e6c9fa15bb5ad90175d0c2c85bd53755a22e
Zach Romitz <fanofbond138@gmail.com>
2017-06-06 16:15:10 -0400

    fixed messed up merge

--- com/laboon/World.java
+++ com/laboon/World.java
@@ -17,11 +17,8 @@ public class World {
 	Cell[][] _world;
 	
 	/**
-<<<<<<< HEAD
-	 * A random number generator (actually pseudo-random, of course).
-=======
-	 * A pseudo-random number generator.
->>>>>>> wjl_display
+	*A random number generator (actually pseudo-random, of course).
+
 	 */
 	
 	Random _rng;
@@ -120,11 +117,10 @@ public class World {
 	 */
 	
 	public String toString() {
-<<<<<<< HEAD
-		String toReturn = "> ";
-=======
+
+
 		String toReturn = "* ";
->>>>>>> wjl_display
+
 		for (int j= 0; j < _size; j++) {
 			toReturn += String.valueOf(j % 10);
 		}
